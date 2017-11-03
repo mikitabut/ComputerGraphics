a05b78febf6eedb268c6c55b72add4d32086c001
minald <minaldandrey@gmail.com>
2017-11-03 03:45:49 +0300

    Add files via upload

---
+++ 2. Drawing lines/2. Drawing lines/DrawingLines.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace _2.Drawing_lines
+{
+    partial class DrawingLines
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.numericUpDownFirstX = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownFirstY = new System.Windows.Forms.NumericUpDown();
+            this.labelFirstX = new System.Windows.Forms.Label();
+            this.labelFirstY = new System.Windows.Forms.Label();
+            this.groupBoxFirstPoint = new System.Windows.Forms.GroupBox();
+            this.groupBoxSecondPoint = new System.Windows.Forms.GroupBox();
+            this.numericUpDownSecondY = new System.Windows.Forms.NumericUpDown();
+            this.labelSecondY = new System.Windows.Forms.Label();
+            this.numericUpDownSecondX = new System.Windows.Forms.NumericUpDown();
+            this.labelSecondX = new System.Windows.Forms.Label();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownFirstX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownFirstY)).BeginInit();
+            this.groupBoxFirstPoint.SuspendLayout();
+            this.groupBoxSecondPoint.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSecondY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSecondX)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.BackColor = System.Drawing.Color.White;
+            this.panel.Location = new System.Drawing.Point(0, 0);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(900, 600);
+            this.panel.TabIndex = 0;
+            this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // numericUpDownFirstX
+            // 
+            this.numericUpDownFirstX.Location = new System.Drawing.Point(55, 26);
+            this.numericUpDownFirstX.Margin = new System.Windows.Forms.Padding(10);
+            this.numericUpDownFirstX.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownFirstX.Name = "numericUpDownFirstX";
+            this.numericUpDownFirstX.Size = new System.Drawing.Size(80, 20);
+            this.numericUpDownFirstX.TabIndex = 1;
+            // 
+            // numericUpDownFirstY
+            // 
+            this.numericUpDownFirstY.Location = new System.Drawing.Point(55, 66);
+            this.numericUpDownFirstY.Margin = new System.Windows.Forms.Padding(10);
+            this.numericUpDownFirstY.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownFirstY.Name = "numericUpDownFirstY";
+            this.numericUpDownFirstY.Size = new System.Drawing.Size(80, 20);
+            this.numericUpDownFirstY.TabIndex = 2;
+            // 
+            // labelFirstX
+            // 
+            this.labelFirstX.AutoSize = true;
+            this.labelFirstX.Location = new System.Drawing.Point(19, 28);
+            this.labelFirstX.Name = "labelFirstX";
+            this.labelFirstX.Size = new System.Drawing.Size(23, 13);
+            this.labelFirstX.TabIndex = 4;
+            this.labelFirstX.Text = "X : ";
+            // 
+            // labelFirstY
+            // 
+            this.labelFirstY.AutoSize = true;
+            this.labelFirstY.Location = new System.Drawing.Point(19, 68);
+            this.labelFirstY.Name = "labelFirstY";
+            this.labelFirstY.Size = new System.Drawing.Size(23, 13);
+            this.labelFirstY.TabIndex = 5;
+            this.labelFirstY.Text = "Y : ";
+            // 
+            // groupBoxFirstPoint
+            // 
+            this.groupBoxFirstPoint.Controls.Add(this.numericUpDownFirstY);
+            this.groupBoxFirstPoint.Controls.Add(this.labelFirstY);
+            this.groupBoxFirstPoint.Controls.Add(this.numericUpDownFirstX);
+            this.groupBoxFirstPoint.Controls.Add(this.labelFirstX);
+            this.groupBoxFirstPoint.Location = new System.Drawing.Point(928, 86);
+            this.groupBoxFirstPoint.Margin = new System.Windows.Forms.Padding(25);
+            this.groupBoxFirstPoint.Name = "groupBoxFirstPoint";
+            this.groupBoxFirstPoint.Size = new System.Drawing.Size(148, 103);
+            this.groupBoxFirstPoint.TabIndex = 6;
+            this.groupBoxFirstPoint.TabStop = false;
+            this.groupBoxFirstPoint.Text = "First point";
+            // 
+            // groupBoxSecondPoint
+            // 
+            this.groupBoxSecondPoint.Controls.Add(this.numericUpDownSecondY);
+            this.groupBoxSecondPoint.Controls.Add(this.labelSecondY);
+            this.groupBoxSecondPoint.Controls.Add(this.numericUpDownSecondX);
+            this.groupBoxSecondPoint.Controls.Add(this.labelSecondX);
+            this.groupBoxSecondPoint.Location = new System.Drawing.Point(928, 207);
+            this.groupBoxSecondPoint.Margin = new System.Windows.Forms.Padding(25);
+            this.groupBoxSecondPoint.Name = "groupBoxSecondPoint";
+            this.groupBoxSecondPoint.Size = new System.Drawing.Size(148, 103);
+            this.groupBoxSecondPoint.TabIndex = 7;
+            this.groupBoxSecondPoint.TabStop = false;
+            this.groupBoxSecondPoint.Text = "Second point";
+            // 
+            // numericUpDownSecondY
+            // 
+            this.numericUpDownSecondY.Location = new System.Drawing.Point(55, 66);
+            this.numericUpDownSecondY.Margin = new System.Windows.Forms.Padding(10);
+            this.numericUpDownSecondY.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownSecondY.Name = "numericUpDownSecondY";
+            this.numericUpDownSecondY.Size = new System.Drawing.Size(80, 20);
+            this.numericUpDownSecondY.TabIndex = 2;
+            // 
+            // labelSecondY
+            // 
+            this.labelSecondY.AutoSize = true;
+            this.labelSecondY.Location = new System.Drawing.Point(19, 68);
+            this.labelSecondY.Name = "labelSecondY";
+            this.labelSecondY.Size = new System.Drawing.Size(23, 13);
+            this.labelSecondY.TabIndex = 5;
+            this.labelSecondY.Text = "Y : ";
+            // 
+            // numericUpDownSecondX
+            // 
+            this.numericUpDownSecondX.Location = new System.Drawing.Point(55, 26);
+            this.numericUpDownSecondX.Margin = new System.Windows.Forms.Padding(10);
+            this.numericUpDownSecondX.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownSecondX.Name = "numericUpDownSecondX";
+            this.numericUpDownSecondX.Size = new System.Drawing.Size(80, 20);
+            this.numericUpDownSecondX.TabIndex = 1;
+            // 
+            // labelSecondX
+            // 
+            this.labelSecondX.AutoSize = true;
+            this.labelSecondX.Location = new System.Drawing.Point(19, 28);
+            this.labelSecondX.Name = "labelSecondX";
+            this.labelSecondX.Size = new System.Drawing.Size(23, 13);
+            this.labelSecondX.TabIndex = 4;
+            this.labelSecondX.Text = "X : ";
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDraw.Location = new System.Drawing.Point(950, 338);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(87, 31);
+            this.buttonDraw.TabIndex = 8;
+            this.buttonDraw.Text = "Draw";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // DrawingLines
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 600);
+            this.Controls.Add(this.buttonDraw);
+            this.Controls.Add(this.groupBoxSecondPoint);
+            this.Controls.Add(this.groupBoxFirstPoint);
+            this.Controls.Add(this.panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(1116, 638);
+            this.MinimumSize = new System.Drawing.Size(1116, 638);
+            this.Name = "DrawingLines";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Drawing lines";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownFirstX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownFirstY)).EndInit();
+            this.groupBoxFirstPoint.ResumeLayout(false);
+            this.groupBoxFirstPoint.PerformLayout();
+            this.groupBoxSecondPoint.ResumeLayout(false);
+            this.groupBoxSecondPoint.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSecondY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSecondX)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.NumericUpDown numericUpDownFirstX;
+        private System.Windows.Forms.NumericUpDown numericUpDownFirstY;
+        private System.Windows.Forms.Label labelFirstX;
+        private System.Windows.Forms.Label labelFirstY;
+        private System.Windows.Forms.GroupBox groupBoxFirstPoint;
+        private System.Windows.Forms.GroupBox groupBoxSecondPoint;
+        private System.Windows.Forms.NumericUpDown numericUpDownSecondY;
+        private System.Windows.Forms.Label labelSecondY;
+        private System.Windows.Forms.NumericUpDown numericUpDownSecondX;
+        private System.Windows.Forms.Label labelSecondX;
+        private System.Windows.Forms.Button buttonDraw;
+    }
+}
+
---
+++ 2. Drawing lines/2. Drawing lines/DrawingLines.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace _2.Drawing_lines
+{
+    public partial class DrawingLines : Form
+    {
+        #region Initialization
+        public const int CellSize = 50;
+        public int panelWidth;
+        public int panelHeight;
+        Point center;
+        Graphics g;
+        Pen thinBlack = new Pen(Color.Black, 1);
+        Pen thickBlack = new Pen(Color.Black, 4);
+        Pen thinRed = new Pen(Color.Red, 2);
+        SolidBrush blackBrush = new SolidBrush(Color.Gray);
+
+        public DrawingLines()
+        {
+            InitializeComponent();
+            panelWidth = panel.Width; //x
+            panelHeight = panel.Height; //y
+            center = new Point(panelWidth / 2, panelHeight / 2);
+            g = panel.CreateGraphics();
+        }
+        #endregion
+
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+            for (int x = CellSize / 2; x < panelWidth; x += CellSize)
+            {
+                g.DrawLine(thinBlack, new Point(x, 0), new Point(x, 600));
+            }
+            for (int y = CellSize / 2; y < panelHeight; y += CellSize)
+            {
+                g.DrawLine(thinBlack, new Point(0, y), new Point(900, y));
+            }
+            g.DrawLine(thickBlack, new Point(center.X, 0), new Point(center.X, panelHeight));
+            g.DrawLine(thickBlack, new Point(0, center.Y), new Point(panelWidth, center.Y));
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            panel.Refresh();
+            Point first = GetFirstPoint();
+            Point second = GetSecondPoint();
+            FillPixels(first, second);
+        }
+
+        private Point GetFirstPoint()
+        {
+            int firstX = Convert.ToInt32(numericUpDownFirstX.Value);
+            int firstY = Convert.ToInt32(numericUpDownFirstY.Value);
+            return new Point(firstX, firstY);
+        }
+
+        private Point GetSecondPoint()
+        {
+            int secondX = Convert.ToInt32(numericUpDownSecondX.Value);
+            int secondY = Convert.ToInt32(numericUpDownSecondY.Value);
+            return new Point(secondX, secondY);
+        }
+
+        private void DrawLine(Point first, Point second)
+        {
+            int firstX = Convert.ToInt32(numericUpDownFirstX.Value);
+            int firstY = Convert.ToInt32(numericUpDownFirstY.Value);
+            int secondX = Convert.ToInt32(numericUpDownSecondX.Value);
+            int secondY = Convert.ToInt32(numericUpDownSecondY.Value);
+            Point center = new Point(panelWidth / 2, panelHeight / 2);
+            Point firstPoint = new Point(center.X + firstX * CellSize, center.Y - firstY * CellSize);
+            Point secondPoint = new Point(center.X + secondX * CellSize, center.Y - secondY * CellSize);
+            g.DrawLine(thinRed, firstPoint, secondPoint);
+        }
+
+        private void FillPixels(Point first, Point second)
+        {
+            double error = -0.5;
+            double k = ((double)(second.Y - first.Y) / (second.X - first.X));
+            int dx = Math.Abs(second.X - first.X);
+            int dy = Math.Abs(second.Y - first.Y);
+            int stepX = Math.Sign(second.X - first.X);
+            int stepY = Math.Sign(second.Y - first.Y);
+            int x = first.X;
+            int y = first.Y;
+            if (dy > dx) k = 1 / k;
+            FillPixel(first);
+            DrawLine(first, second);
+            for (int i = 1; i <= Math.Max(dx, dy); i++)
+            {
+                error += Math.Abs(k);
+                if(dy > dx)
+                {
+                    y += stepY;
+                    if (error >= 0)
+                    {
+                        x += stepX;
+                        error--;
+                    }
+                }
+                else
+                {
+                    x += stepX;
+                    if (error >= 0)
+                    {
+                        y += stepY;
+                        error--;
+                    }
+                }
+
+                Thread.Sleep(1000);
+                FillPixel(new Point(x, y));
+                DrawLine(first, second);
+            }
+        }
+
+        private void FillPixel(Point point)
+        {
+            int left = (int)(center.X + (point.X - 0.5) * CellSize);
+            int top = (int)(center.Y - (point.Y + 0.5) * CellSize);
+            g.FillRectangle(blackBrush, left, top, CellSize, CellSize);
+        }
+    }
+}
